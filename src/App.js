35e3dead7242b54cd343d07f5d120aff82f71547
Vladislav Medvedi <desmo_papa@Vladislavs-MacBook-Pro.local>
2020-04-21 22:09:11 +0200

    main datas on air

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { fetchData } from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import { Cards, Chart, CountryPicker } from './components';
+import s from './App.module.css';
+
+class App extends React.Component {
+
+    state = {
+        data: {}
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData });
+    }
+
+    render() {
+        const { data } = this.state;
+        return (
+            <div className={s.container}>
+                <Cards data={data}/>
+                <CountryPicker />
+                <Chart />
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import s from "./Cards.module.css";
+import CountUp from "react-countup";
+import cx from 'classnames';
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading...";
+  }
+  return (
+    <div className={s.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card} xs={12} md={3} className={cx(s.card, s.infected)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card} xs={12} md={3} className={cx(s.card, s.recovered)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">
+              Number of recoveries from COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card} xs={12} md={3} className={cx(s.card, s.deaths)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">
+              Number of deaths caused by COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Cards;
